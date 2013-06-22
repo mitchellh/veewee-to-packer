76087b124c3dd41c1b56b5188a957ab3e6ae5409
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-06-22 00:12:46 -0700

    Command-line options

--- lib/veewee-to-packer.rb
+++ lib/veewee-to-packer.rb
@@ -4,12 +4,15 @@ require "veewee-to-packer/version"
 
 module VeeweeToPacker
   # Converts the given Veewee template into a Packer template, outputting
-  # the JSON to the given output path.
-  def self.convert(input, output)
+  # the JSON to the given output path. The builders that the template will
+  # contain is specified by `builders`.
+  def self.convert(input, output, builders)
     begin
       load input
     rescue LoadError => e
       raise Error, "Error loading input template: #{e}"
     end
+
+    p Veewee::Definition.captured
   end
 end
